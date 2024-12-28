3e176be36327e201b2e5fa17fe05fa5d212466ad
alexrhogue <Alex.R.Hogue@gmail.com>
2024-12-27 19:29:32 -0800

    use linear equations

--- 13/main.py
+++ 13/main.py
@@ -14,67 +14,27 @@ def load_input(input_file: str) -> list[str]:
 
 	return lines
 
-
-""" def solve_machine(aX, aY, bX, bY, curX, curY, targetX, targetY):
-	print(aX, aY, bX, bY, curX, curY, targetX, targetY)
-
-	if curX == targetX and curY == targetY:
-		return 0
-	
-	if curX > targetX or curY > targetY:
-		return -1
-	
-	a = solve_machine(aX, aY, bX, bY, curX + aX, curY + aY, targetX, targetY)
-	b = solve_machine(aX, aY, bX, bY, curX + bX, curY + bY, targetX, targetY)
-
-	if a == -1 and b == -1:
-		return -1
-	
-	if a == -1:
-		return b + 3
-	
-	if b == -1:
-		return a + 1
-	
-	a += 1
-	b += 3
-
-	return a if a < b else b """
-
 def solve_machine(aX, aY, bX, bY, targetX, targetY):
-	numA = round(min(targetX / aX, targetY / aY))
-	numB = 0
-	cost = -1
+	numA = (targetX * bY - bX * targetY) / (aX * bY - bX * aY)
+	numB = (aX * targetY - targetX * aY) / (aX * bY - bX * aY)
 
-	while(numA >= 0):
-		curX = numA * aX + numB * bX 
-		curY = numA * aY + numB * bY
-
-		if numA == 80:
-			print(numA,numB)
-			print(curX,curY)
-
-		if curX == targetX and curY == targetY:
-			new_cost = (3 * numA) + numB
-			print(new_cost)
-			if numA <= 100 and numB <= 100 and (cost == -1 or new_cost < cost):
-				cost = new_cost
-
-		if curX < targetX or curY < targetY:
-			numB += 1
-		else:
-			numA -= 1
-
-	return cost
+	if numA.is_integer() and numB.is_integer():
+		return 3 * int(numA) + int(numB)
+	
+	return 0
 
 
 
 def main():
 	machines = load_input("input.txt")
 
+	pt_2 = True
 	cost_to_win = 0
 	for machine in machines:
 		a, b, target = machine
+		if pt_2:
+			target[0] += 10000000000000
+			target[1] += 10000000000000
 		cost = solve_machine(a[0], a[1], b[0], b[1], target[0], target[1])
 
 		if cost > -1:
